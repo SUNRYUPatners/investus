9496bb538400a0b02e18d21a181c917251927667
RYU-MacBook Air15 <ryu-macmini@MacBook-Air.local>
2026-06-23 15:40:07 +0900

    chore: sw.js 업데이트

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "investus-v1782085825863";
+const CACHE = "investus-v1782170126982";
 
 self.addEventListener("install", (e) => {
   // Activate immediately — don't wait for old tabs to close
