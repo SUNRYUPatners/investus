31aae98389f0f975f87282a8e7aa52bd12c2ccad
RYU-MacBook Air15 <ryu-macmini@MacBook-Air.local>
2026-08-11 09:45:23 +0900

    chore: sw.js cache bump

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "investus-v1786406392026";
+const CACHE = "investus-v1786409059887";
 
 self.addEventListener("install", (e) => {
   // Activate immediately — don't wait for old tabs to close
